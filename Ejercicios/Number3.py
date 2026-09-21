00800cea9d0483457de9fee21360ce30fbc37c18
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:06:24 -0500

    Update Number3.py

--- Ejercicios/Number3.py
+++ Ejercicios/Number3.py
@@ -16,4 +16,40 @@ carro.agregar_articulo("zapatilla", 20)
 carro.agregar_articulo("bola", 30)
 carro.agregar_articulo("zapatilla", 40)
 print(carro.total_carrito())
-print(carro.articulos_por_rango(20, 30))    
+print(carro.articulos_por_rango(20, 30))  
+#Hecho por mi 
+class Libros:
+
+    def __init__(self):
+        self.libros = {}
+
+    def agregar(self, nombre, precio):
+        self.libros[nombre] = precio
+
+    def total(self):
+        total = 0
+
+        for precio in self.libros.values():
+            total += precio
+
+        return total
+
+    def buscar(self, minimo, maximo):
+        resultado = []
+
+        for nombre, precio in self.libros.items():
+            if minimo <= precio <= maximo:
+                resultado.append(nombre)
+
+        return resultado
+
+
+l = Libros()
+
+l.agregar("La culpa es de la vaca", 10)
+l.agregar("Abitos Atomicos", 15)
+l.agregar("Mujercitas", 8)
+
+print(l.libros)
+print(l.total())
+print(l.buscar(8, 10))
