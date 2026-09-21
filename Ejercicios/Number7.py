69bc58ef1a34640db5b2d500eb6e7a24d25cca1c
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:25:23 -0500

    Implement Mascotas class for pet management
    Added a new class 'Mascotas' to manage pet ages and calculate averages.

--- Ejercicios/Number7.py
+++ Ejercicios/Number7.py
@@ -16,3 +16,36 @@ gestor.agregar_persona("Maria", 40)
 gestor.agregar_persona("Luis", 50)
 print(gestor.personas_mayores(30))
 print(gestor.edad_promedio())   
+
+#Hecho  por mi 
+
+class Mascotas:
+
+    def __init__(self):
+        self.mascotas = {}
+
+    def agregar(self, nombre, edad):
+        self.mascotas[nombre] = edad
+
+    def mayores(self, edad):
+        resultado = []
+
+        for nombre, edad_mascota in self.mascotas.items():
+            if edad_mascota >= edad:
+                resultado.append(nombre)
+
+        return resultado
+
+    def promedio(self):
+        return sum(self.mascotas.values()) / len(self.mascotas)
+
+
+m = Mascotas()
+
+m.agregar("Kaory", 5)
+m.agregar("Max", 2)
+m.agregar("Laica", 7)
+
+print(m.mascotas)
+print(m.mayores(5))
+print(m.promedio())
