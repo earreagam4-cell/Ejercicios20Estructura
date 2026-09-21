1676b29f56c5c9682a31c9ba20614e93ebad78c6
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:28:56 -0500

    Update Number12.py

--- Ejercicios/Number12.py
+++ Ejercicios/Number12.py
@@ -13,3 +13,33 @@ class SelectorRango:
 
 selector = SelectorRango()
 print(selector.elementos_en_multiples_rangos((1, 10), (2, 5), (3, 8), (4, 9), (5, 12), (6, 15), (7, 18), (8, 20), (9, 25), (10, 30)))       
+
+# Hecho por mi 
+
+class Rangos:
+
+    def __init__(self):
+        self.rangos = []
+
+    def crear(self, inicio, fin):
+        numeros = []
+
+        for numero in range(inicio, fin + 1):
+            numeros.append(numero)
+
+        return tuple(numeros)
+
+    def unir(self, *rangos):
+        resultado = set()
+
+        for rango in rangos:
+            for numero in range(rango[0], rango[1] + 1):
+                resultado.add(numero)
+
+        return list(resultado)
+
+
+r = Rangos()
+
+print(r.crear(1, 4))
+print(r.unir((1, 3), (3, 5)))
