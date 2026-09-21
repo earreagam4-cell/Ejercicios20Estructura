d81c35d92c7fc0d9679bbbf452082aad7166e7bb
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:27:38 -0500

    Update Number10.py

--- Ejercicios/Number10.py
+++ Ejercicios/Number10.py
@@ -20,3 +20,42 @@ tareas.agregar_tarea("Tarea 5", 5)
 print(tareas.tareas_prioritarias())
 tareas.eliminar_completada("Tarea 1")
 print(tareas.tareas_prioritarias())
+
+#Hecho por mi
+
+class Compras:
+
+    def __init__(self):
+        self.compras = []
+
+    def agregar(self, producto, cantidad):
+        self.compras.append((producto, cantidad))
+
+    def mayores(self):
+        resultado = []
+
+        for compra in self.compras:
+            if compra[1] >= 3:
+                resultado.append(compra)
+
+        return resultado
+
+    def eliminar(self, producto):
+        for compra in self.compras:
+            if compra[0] == producto:
+                self.compras.remove(compra)
+                return
+
+
+c = Compras()
+
+c.agregar("Bread", 5)
+c.agregar("Milk", 2)
+c.agregar("Rice", 4)
+
+print(c.compras)
+print(c.mayores())
+
+c.eliminar("Bread")
+
+print(c.compras)
