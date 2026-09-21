0e04f0c30679975477c0ec0cb20133a9af9f989f
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:29:25 -0500

    Introduce Mezclador class for list mixing
    Added a new class 'Mezclador' to mix multiple lists together.

--- Ejercicios/Number13.py
+++ Ejercicios/Number13.py
@@ -13,3 +13,32 @@ class CombinadorListas:
 combinador = CombinadorListas()
 print(combinador.intercalar([1, 2, 3], [4, 5, 6]))
 print(combinador.intercalar_multiples([1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12])) 
+
+#Hecho por mi 
+
+class Mezclador:
+
+    def __init__(self):
+        self.resultados = []
+
+    def mezclar(self, lista1, lista2):
+        resultado = []
+
+        for i in range(len(lista1)):
+            resultado.append(lista1[i])
+            resultado.append(lista2[i])
+
+        return resultado
+
+    def mezclar_muchas(self, *listas):
+        resultado = list(listas[0])
+
+        for lista in listas[1:]:
+            resultado = self.mezclar(resultado, lista)
+
+        return resultado
+
+
+m = Mezclador()
+
+print(m.mezclar(["A", "B"], ["C", "D"]))
