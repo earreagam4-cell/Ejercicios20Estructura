68321111cd3b8f0dfda4b7cc18d5e57236a6af0f
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:26:26 -0500

    Introduce Cursos class for course management
    Added a new class 'Cursos' to manage courses and students.

--- Ejercicios/Number8.py
+++ Ejercicios/Number8.py
@@ -20,3 +20,39 @@ equipos.agregar_jugador("Equipo 1", "Jugador 2")
 equipos.agregar_jugador("Equipo 2", "Jugador 3")
 equipos.agregar_jugador("Equipo 3", "Jugador 4")
 print(equipos.equipo_mayor_integrantes())   
+
+#Hecho por mi 
+class Cursos:
+
+    def __init__(self):
+        self.cursos = {}
+
+    def crear(self, curso):
+        self.cursos[curso] = []
+
+    def agregar_estudiante(self, curso, estudiante):
+        self.cursos[curso].append(estudiante)
+
+    def curso_mayor(self):
+        mayor = ""
+        cantidad = 0
+
+        for curso, estudiantes in self.cursos.items():
+            if len(estudiantes) > cantidad:
+                cantidad = len(estudiantes)
+                mayor = curso
+
+        return mayor
+
+
+c = Cursos()
+
+c.crear("Python")
+c.crear("Java")
+
+c.agregar_estudiante("Python", "Ana")
+c.agregar_estudiante("Python", "Luis")
+c.agregar_estudiante("Java", "Pedro")
+
+print(c.cursos)
+print(c.curso_mayor())
