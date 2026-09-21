bae21cfc8ae30a8892168ff4999b2a36e27ce573
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:04:21 -0500

    Add Frutas class for fruit management
    Added a new class 'Frutas' to manage a collection of fruits, allowing addition of multiple fruits and counting unique entries.

--- Ejercicios/Number2.py
+++ Ejercicios/Number2.py
@@ -20,4 +20,31 @@ class AnalizadorTexto:
 analizador = AnalizadorTexto()
 analizador.agregar_multiples("hola", "hola", "adios", "adios", "adios", "hola")
 print(analizador.contar_palabras())
-print(analizador.palabras_ordenadas)                
+print(analizador.palabras_ordenadas) 
+
+#Hecho por mi 
+class Frutas:
+
+    def __init__(self):
+        self.frutas = []
+        self.unicas = set()
+
+    def agregar(self, fruta):
+        self.frutas.append(fruta)
+        self.unicas.add(fruta)
+
+    def agregar_muchas(self, *frutas):
+        for fruta in frutas:
+            self.agregar(fruta)
+
+    def cantidad(self):
+        return len(self.unicas)
+
+
+f = Frutas()
+
+f.agregar_muchas("manzana", "pera", "manzana", "uva")
+
+print(f.frutas)
+print(f.unicas)
+print(f.cantidad())
