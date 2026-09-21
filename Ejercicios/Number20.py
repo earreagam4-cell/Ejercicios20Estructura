2f4fc663e44f561a0478244fa29619868f86a584
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:33:19 -0500

    Update Number20.py

--- Ejercicios/Number20.py
+++ Ejercicios/Number20.py
@@ -19,3 +19,53 @@ analizador = AnalizadorPatrones()
 print(analizador.encontrar_palabras("hola hola adios adios", "hola"))
 print(analizador.agrupar_por_longitud("hola hola adios adios"))
 print(analizador.palabras_unicas())     
+
+# Hecho por mi 
+
+class Buscador:
+
+    def __init__(self):
+        self.palabras = []
+
+    def buscar(self, texto, inicio):
+
+        resultado = []
+
+        for palabra in texto.split():
+
+            if palabra.startswith(inicio):
+                resultado.append(palabra)
+
+        return resultado
+
+    def agrupar(self, texto):
+
+        resultado = {}
+
+        for palabra in texto.split():
+
+            largo = len(palabra)
+
+            if largo not in resultado:
+                resultado[largo] = []
+
+            resultado[largo].append(palabra)
+
+        return resultado
+
+    def unicas(self):
+
+        return set(self.palabras)
+
+
+b = Buscador()
+
+texto = "casa carro perro casa"
+
+print(b.buscar(texto, "ca"))
+
+print(b.agrupar(texto))
+
+b.palabras = texto.split()
+
+print(b.unicas())
