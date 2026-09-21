970f5cad93c1f9e0fbf7e83b2ba79a80ef1ec337
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:27:06 -0500

    Update Number9.py

--- Ejercicios/Number9.py
+++ Ejercicios/Number9.py
@@ -20,3 +20,44 @@ class AnalizadorString:
 analizador = AnalizadorString("hola")
 print(analizador.contar_por_tipo("hola"))
 print(analizador.mas_largo())       
+
+#Hecho por mi
+
+
+    def __init__(self):
+        self.texto_largo = ""
+
+    def es_vocal(self, letra):
+        return letra.lower() in "aeiou"
+
+    def contar(self, texto):
+        vocales = 0
+        consonantes = 0
+        numeros = 0
+
+        if len(texto) > len(self.texto_largo):
+            self.texto_largo = texto
+
+        for letra in texto:
+
+            if letra.isdigit():
+                numeros += 1
+
+            elif letra.isalpha():
+
+                if self.es_vocal(letra):
+                    vocales += 1
+                else:
+                    consonantes += 1
+
+        return {
+            "vocales": vocales,
+            "consonantes": consonantes,
+            "numeros": numeros
+        }
+
+
+l = Letras()
+
+print(l.contar("Casa123"))
+print(l.texto_largo)
