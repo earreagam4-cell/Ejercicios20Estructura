5ee2323246ee95a869813ed8f837f5160f997d53
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:24:50 -0500

    Update Number6.py

--- Ejercicios/Number6.py
+++ Ejercicios/Number6.py
@@ -20,3 +20,33 @@ gestor.registrar_multiples(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 print(gestor.minima())
 print(gestor.maxima())
 print(gestor.promedio())    
+
+#Hecho por mi 
+
+class Calificaciones:
+
+    def __init__(self):
+        self.calificaciones = []
+
+    def agregar(self, *notas):
+        for nota in notas:
+            self.calificaciones.append(nota)
+
+    def mayor(self):
+        return max(self.calificaciones)
+
+    def menor(self):
+        return min(self.calificaciones)
+
+    def promedio(self):
+        return sum(self.calificaciones) / len(self.calificaciones)
+
+
+c = Calificaciones()
+
+c.agregar(80, 90, 70, 85)
+
+print(c.calificaciones)
+print(c.mayor())
+print(c.menor())
+print(c.promedio())
