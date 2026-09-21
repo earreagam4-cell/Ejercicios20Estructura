3bfbb2bb25fe0d64d73b40a6aad5d5f791c232b7
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:08:13 -0500

    Implement Inversor class for list inversion
    Added a new Inversor class for sequence inversion with methods to invert single and multiple lists.

--- Ejercicios/Number4.py
+++ Ejercicios/Number4.py
@@ -17,3 +17,32 @@ class InversorSecuencia:
 invertido = InversorSecuencia()
 print(invertido.invertir_lista([1, 2, 3, 4, 5]))
 print(invertido.invertir_multiples([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]))
+
+#Hecho por mi 
+
+class Inversor:
+
+    def __init__(self):
+        self.resultados = []
+
+    def invertir(self, lista):
+        resultado = []
+
+        for i in range(len(lista) - 1, -1, -1):
+            resultado.append(lista[i])
+
+        return resultado
+
+    def invertir_muchas(self, *listas):
+        resultado = {}
+
+        for lista in listas:
+            resultado[tuple(lista)] = self.invertir(lista)
+
+        return resultado
+
+
+i = Inversor()
+
+print(i.invertir(["Mixi", "Matias", "Pedro"]))
+print(i.invertir_muchas(["Mixi", "Matias"], ["Juan", "Kelly"]))
