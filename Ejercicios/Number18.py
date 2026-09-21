54302d044c082c56db732b4dbd2c68c16c746be7
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:32:14 -0500

    Update Number18.py

--- Ejercicios/Number18.py
+++ Ejercicios/Number18.py
@@ -11,3 +11,33 @@ class CalculadorDistancia:
 
 calculador = CalculadorDistancia()
 print(calculador.punto_mas_cercano((0, 0), (1, 1), (2, 2), (3, 3), (4, 4)))     
+
+# Hecho por mi 
+
+import math
+
+class Distancia:
+
+    def __init__(self):
+        self.distancias = []
+
+    def calcular(self, punto1, punto2):
+
+        x1 = punto1[0]
+        y1 = punto1[1]
+
+        x2 = punto2[0]
+        y2 = punto2[1]
+
+        distancia = math.sqrt(
+            (x2 - x1) ** 2 + (y2 - y1) ** 2
+        )
+
+        self.distancias.append(distancia)
+
+        return distancia
+
+
+d = Distancia()
+
+print(d.calcular((0, 0), (3, 4)))
