a33b66e94eda64dc9e6e544afb96c5cbd491d9e8
earreagam4-cell <earreagam4@unemi.edu.ec>
2026-09-21 10:31:30 -0500

    Update Number17.py

--- Ejercicios/Number17.py
+++ Ejercicios/Number17.py
@@ -28,3 +28,38 @@ class AgrupadorEdades:
 agrupador = AgrupadorEdades()
 agrupador.agrupar_por_categoria(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
 print(agrupador.edad_promedio_categoria("adolescente"))
+
+# Hecho por mi 
+
+class Clasificador:
+
+    def __init__(self):
+        self.grupos = {
+            "niño": [],
+            "joven": [],
+            "adulto": []
+        }
+
+    def clasificar(self, edad):
+
+        if edad < 13:
+            return "niño"
+
+        elif edad < 18:
+            return "joven"
+
+        else:
+            return "adulto"
+
+    def agrupar(self, *edades):
+
+        for edad in edades:
+            grupo = self.clasificar(edad)
+            self.grupos[grupo].append(edad)
+
+        return self.grupos
+
+
+c = Clasificador()
+
+print(c.agrupar(10, 15, 20, 30))
